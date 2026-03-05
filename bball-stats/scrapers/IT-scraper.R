259f627fb0915312f05dc56f7feb6e6ece8c4f03
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2026-03-05 09:59:08 +0200

    Serie A scraper.
    Updated player and team data extraction to ensure team names are in uppercase when saving to CSV.

--- bball-stats/scrapers/IT-scraper.R
+++ bball-stats/scrapers/IT-scraper.R
@@ -126,7 +126,8 @@ rm(list=setdiff(ls(),c("PP","TT")))
 
 # beepr::beep()
 # write files in .csv format
-write.csv(bind_rows(PP),"bball-stats/data/IT-players.csv")
+write.csv(bind_rows(PP) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/IT-players.csv")
 
-write.csv(bind_rows(TT),"bball-stats/data/IT-teams.csv")
+write.csv(bind_rows(TT) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/IT-teams.csv")
+
 
