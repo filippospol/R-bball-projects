0106372c3a66134621c666576db75ad0819bf26e
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2026-03-12 00:33:31 +0200

    Update IT-scraper.R

--- bball-stats/scrapers/IT-scraper.R
+++ bball-stats/scrapers/IT-scraper.R
@@ -51,76 +51,87 @@ fixture_info = fixture_info %>%
 
 #' *LOOP OVER MATCH ID'S AND GET BOXSCORES*
 
+#' *DYNAMICALLY FETCH TODAY'S NEXT.JS BUILD ID*
+main_page = read_html("https://www.legabasket.it")
+next_data_json = main_page %>% html_node("#__NEXT_DATA__") %>% html_text()
+build_id = fromJSON(next_data_json)$buildId
+
+
+#' *LOOP OVER MATCH ID'S AND GET BOXSCORES*
 PP = list()
 TT = list()
-for (i in 1:dim(fixture_info)[1]) {
+
+print("Starting boxscore extraction loop...")
+
+for (i in 1:nrow(fixture_info)) {
   
-  if (ymd(fixture_info$GAME_DATE[i])>=today()) break
+  # Be a polite bot
+  Sys.sleep(0.5)
   
-  game_id = fixture_info[i,1]
-  # URL (JSON):
-  fixture_url = "https://www.legabasket.it/_next/data/LJ_cpxPspBbJ6PZKqa8gR/game/{game_id}/tabellini.json?id={game_id}&detail=tabellini" %>% 
-    glue()
+  if (ymd(fixture_info$GAME_DATE[i]) >= today()) break
   
-  # API data:
-  raw_json = suppressMessages(
-    fromJSON(content(GET(fixture_url), "text"))
-  )
+  # FIX 1: Properly extract the game ID as a single character/numeric value
+  game_id = fixture_info$GAME_ID[i]
   
-  # Player Stats:
-  PP[[i]] = bind_rows(
-    raw_json$pageProps$game$scores$ht$rows %>% 
-      as_tibble() %>% 
-      clean_names("all_caps") %>% 
-      rename(MIN2=MIN) %>% 
-      mutate(PLAYER=paste0(PLAYER_NAME," ",PLAYER_SURNAME),
-             TEAM=fixture_info$H_TEAM_NAME[i],MATCHUP=fixture_info$MATCHUP[i],
-             MIN=MIN2,PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
-             FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
-             AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C,
-             SEASON=fixture_info$SEASON[i],LEAGUE=fixture_info$LEAGUE[i],
-             GAME_ID=fixture_info$GAME_ID[i]),
-    raw_json$pageProps$game$scores$vt$rows %>% 
-      as_tibble() %>% 
-      clean_names("all_caps") %>% 
-      rename(MIN2=MIN) %>% 
-      mutate(PLAYER=paste0(PLAYER_NAME," ",PLAYER_SURNAME),
-             TEAM=fixture_info$V_TEAM_NAME[i],MATCHUP=fixture_info$MATCHUP[i],
-             MIN=MIN2,PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
-             FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
-             AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C,
-             SEASON=fixture_info$SEASON[i],LEAGUE=fixture_info$LEAGUE[i],
-             GAME_ID=fixture_info$GAME_ID[i])
-  ) %>% 
-    select(54,52,53,33:51) %>% 
-    mutate(PLAYER=toupper(as.character(PLAYER)),
-           PLAYER = stri_trans_general(PLAYER, "latin-ascii")) %>%
-    mutate(MIN=round(MIN)) %>% 
-    # if minutes is NA, player DNP so remove that row altogether?
-    filter(!is.na(MIN))
+  # FIX 2: Inject the dynamic build_id into the URL
+  fixture_url = glue("https://www.legabasket.it/_next/data/{build_id}/game/{game_id}/tabellini.json?id={game_id}&detail=tabellini")
   
-  # Team Stats:
-  TT[[i]] = bind_rows(
-    raw_json$pageProps$game$scores$ht$totals %>% 
-      as_tibble() %>% 
-      clean_names("all_caps") %>% 
-      mutate(TEAM=fixture_info$H_TEAM_NAME[i],CODE=fixture_info$H_CLUB_CODE[i],
-             MATCHUP=fixture_info$MATCHUP[i]) %>% 
-      select(TEAM,CODE,MATCHUP,
-             PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
-             FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
-             AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C),
-    raw_json$pageProps$game$scores$vt$totals %>% 
-      as_tibble() %>% 
-      clean_names("all_caps") %>% 
-      mutate(TEAM=fixture_info$V_TEAM_NAME[i],CODE=fixture_info$V_CLUB_CODE[i],
-             MATCHUP=fixture_info$MATCHUP[i]) %>% 
-      select(TEAM,CODE,MATCHUP,
-             PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
-             FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
-             AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C)
-  )
+  # Fetch data with a safety net (so one broken game doesn't ruin the whole loop)
+  res = GET(fixture_url)
   
+  if (status_code(res) == 200) {
+    
+    # Only parse if we confirm it is actual JSON text
+    raw_text <- content(res, "text", encoding = "UTF-8")
+    
+    if (str_starts(str_trim(raw_text), "\\{")) {
+      raw_json <- suppressMessages(fromJSON(raw_text))
+      
+      # Player Stats:
+      PP[[i]] = bind_rows(
+        raw_json$pageProps$game$scores$ht$rows %>% as_tibble() %>% clean_names("all_caps") %>%
+          rename(MIN2=MIN) %>% mutate(PLAYER=paste0(PLAYER_NAME," ",PLAYER_SURNAME),
+                                      TEAM=fixture_info$H_TEAM_NAME[i],MATCHUP=fixture_info$MATCHUP[i],
+                                      MIN=MIN2,PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
+                                      FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
+                                      AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C,
+                                      SEASON=fixture_info$SEASON[i],LEAGUE=fixture_info$LEAGUE[i],
+                                      GAME_ID=fixture_info$GAME_ID[i]),
+        raw_json$pageProps$game$scores$vt$rows %>% as_tibble() %>% clean_names("all_caps") %>%
+          rename(MIN2=MIN) %>% mutate(PLAYER=paste0(PLAYER_NAME," ",PLAYER_SURNAME),
+                                      TEAM=fixture_info$V_TEAM_NAME[i],MATCHUP=fixture_info$MATCHUP[i],
+                                      MIN=MIN2,PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
+                                      FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
+                                      AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C,
+                                      SEASON=fixture_info$SEASON[i],LEAGUE=fixture_info$LEAGUE[i],
+                                      GAME_ID=fixture_info$GAME_ID[i])
+      ) %>%
+        select(54,52,53,33:51) %>%
+        mutate(PLAYER=toupper(as.character(PLAYER)),
+               PLAYER = stri_trans_general(PLAYER, "latin-ascii")) %>%
+        mutate(MIN=round(MIN)) %>%
+        filter(!is.na(MIN))
+      
+      # Team Stats:
+      TT[[i]] = bind_rows(
+        raw_json$pageProps$game$scores$ht$totals %>% as_tibble() %>% clean_names("all_caps") %>%
+          mutate(TEAM=fixture_info$H_TEAM_NAME[i],CODE=fixture_info$H_CLUB_CODE[i], MATCHUP=fixture_info$MATCHUP[i]) %>%
+          select(TEAM,CODE,MATCHUP, PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
+                 FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
+                 AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C),
+        raw_json$pageProps$game$scores$vt$totals %>% as_tibble() %>% clean_names("all_caps") %>%
+          mutate(TEAM=fixture_info$V_TEAM_NAME[i],CODE=fixture_info$V_CLUB_CODE[i], MATCHUP=fixture_info$MATCHUP[i]) %>%
+          select(TEAM,CODE,MATCHUP, PTS=PUN,`2PM`=T2_R,`2PA`=T2_T,`3PM`=T3_R,`3PA`=T3_T,
+                 FTM=TL_R,FTA=TL_T,DREB=RIMBALZI_D,OREB=RIMBALZI_O,REB=RIMBALZI_T,
+                 AST=ASS,STL=PALLE_R,BLK=STOPPATE_DAT,TOV=PALLE_P,PF=FALLI_C)
+      )
+      
+    } else {
+      print(paste("Skipping game", game_id, "- Server returned HTML instead of JSON."))
+    }
+  } else {
+    print(paste("Skipping game", game_id, "- API Error:", status_code(res)))
+  }
 }
 rm(list=setdiff(ls(),c("PP","TT")))
 
@@ -129,6 +140,3 @@ rm(list=setdiff(ls(),c("PP","TT")))
 write.csv(bind_rows(PP) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/IT-players.csv")
 
 write.csv(bind_rows(TT) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/IT-teams.csv")
-
-
-
