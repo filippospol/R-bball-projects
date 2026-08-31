a1f6c958dc376debfd0b3433b63324ab33bcc6aa
Filippos Polyzos <61119617+filippospol@users.noreply.github.com>
2026-08-31 13:55:20 +0300

    Eurocup scraper.

--- bball-stats/scrapers/EC-scraper.R
+++ bball-stats/scrapers/EC-scraper.R
@@ -25,31 +25,30 @@ library(rvest)
 #' *EXTRACT MATCH ID'S AND INFORMATION*
 
 # season code:
-# 180 RS games
-# Eight-finals (elimination games to advance to the playoffs)
-# playoffs: best of 3 series
+# 20 teams, they play each other twice, so 19*2=38 rounds and 38*10=380 RS games
+# 3 play-in games
+# playoffs: best of 5 series
+# final four: semi finals and first place game
 league="Eurocup" ; season = "2025-26" ; scode = "U2025" ; games_n = (19*10)+5
 
-GG = list()
-for (i in 1:games_n) {
-  
-  # Overview tab:
-  # Date, Team names and codes from here:
-  overview_url = glue("https://live.euroleague.net/api/Header?gamecode={i}&seasoncode={scode}")
-  res = GET(overview_url)
-  if (content(res, "text", encoding = "UTF-8")=="") next
-  
-  raw_json = fromJSON(content(res, "text", encoding = "UTF-8"))
-  GG[[i]] = raw_json %>% as_tibble() %>% mutate(Game_id=i, .before=1)
-  if (i==85) Sys.sleep(5)
-}
-rm(overview_url,res,raw_json,i)
-fixture_info = bind_rows(GG) %>% 
-  mutate(SEASON=season,LEAGUE=league, .before=2) %>% 
-  mutate(GAME_DATE=dmy(Date),
-         HOME_CODE=TVCodeA,AWAY_CODE=TVCodeB,HOME_TEAM=TeamA,AWAY_TEAM=TeamB,
+games_url = "https://feeds.incrowdsports.com/provider/euroleague-feeds/v2/competitions/{substr(scode,1,1)}/seasons/{scode}/games" %>% 
+  glue()
+res  = GET(games_url, query = list(limit = 600))
+game_list = fromJSON(content(res, "text", encoding = "UTF-8"), flatten = TRUE)
+
+raw_fixtures = game_list$data %>% 
+  as_tibble() %>% 
+  select(where(~!is.list(.)))
+
+fixture_info = raw_fixtures %>% 
+  clean_names("all_caps") %>% 
+  filter(STATUS == "result") %>% 
+  mutate(SEASON=season,LEAGUE=league,
+         GAME_DATE=as.Date(DATE),
+         HOME_TEAM=HOME_ABBREVIATED_NAME,AWAY_TEAM=AWAY_ABBREVIATED_NAME,
          MATCHUP=paste0(GAME_DATE,", ",HOME_CODE," vs ",AWAY_CODE)) %>% 
-  select(GAME_ID=Game_id,SEASON,LEAGUE,MATCHUP,HOME_TEAM,HOME_CODE,AWAY_TEAM,AWAY_CODE)
+  select(GAME_ID=CODE,SEASON,LEAGUE,MATCHUP,HOME_TEAM,HOME_CODE,AWAY_TEAM,AWAY_CODE) %>% 
+  arrange(GAME_ID)
 
 # Clear environment:
 rm(list=setdiff(ls(),c("fixture_info","scode")))
@@ -61,21 +60,27 @@ PP = list()
 TT = list()
 for(i in 1:dim(fixture_info)[1]) {
   
+  Sys.sleep(1)
   boxscore_url = glue("https://live.euroleague.net/api/Boxscore?gamecode={fixture_info$GAME_ID[i]}&seasoncode={scode}")
   res = GET(boxscore_url)
+  # message(paste0("Game id ",fixture_info$GAME_ID[i]),": status code ",res$status_code)
   raw_json = fromJSON(content(res, "text", encoding = "UTF-8"))
   
-   PP[[i]] = suppressWarnings(
+  PP[[i]] = suppressWarnings(
     raw_json %>% 
       pluck("Stats") %>% 
       as_tibble() %>% 
+      # team code, so the cleanup at the end has a stable key to group on.
+      # same home-then-away assumption the TT block below already makes
+      mutate(CODE = c(fixture_info$HOME_CODE[i],fixture_info$AWAY_CODE[i]),
+             TEAM2 = c(fixture_info$HOME_TEAM[i],fixture_info$AWAY_TEAM[i])) %>% 
       unnest() %>% 
       clean_names("all_caps") %>% 
       mutate(GAME_ID=fixture_info$GAME_ID[i],SEASON=fixture_info$SEASON %>% unique(),
              LEAGUE=fixture_info$LEAGUE %>% unique(),MATCHUP=fixture_info$MATCHUP[i],
              PLAYER = str_replace(PLAYER, "^(.*),\\s*(.*)$", "\\2 \\1"),
              MINUTES = round(period_to_seconds(ms(MINUTES)) / 60)) %>% 
-      select(GAME_ID,SEASON,LEAGUE,PLAYER,TEAM,MATCHUP,MIN=MINUTES,PTS=POINTS,`2PM`=FIELD_GOALS_MADE2,
+      select(GAME_ID,SEASON,LEAGUE,PLAYER,TEAM=TEAM2,CODE,MATCHUP,MIN=MINUTES,PTS=POINTS,`2PM`=FIELD_GOALS_MADE2,
              `2PA`=FIELD_GOALS_ATTEMPTED2,`3PM`=FIELD_GOALS_MADE3,`3PA`=FIELD_GOALS_ATTEMPTED3,FTM=FREE_THROWS_MADE,
              FTA=FREE_THROWS_ATTEMPTED,DREB=DEFENSIVE_REBOUNDS,OREB=OFFENSIVE_REBOUNDS,REB=TOTAL_REBOUNDS,AST=ASSISTANCES,STL=STEALS,
              TOV=TURNOVERS,BLK=BLOCKS_FAVOUR,PF=FOULS_COMMITED) %>% 
@@ -94,16 +99,15 @@ for(i in 1:dim(fixture_info)[1]) {
              FTA=FREE_THROWS_ATTEMPTED,DREB=DEFENSIVE_REBOUNDS,OREB=OFFENSIVE_REBOUNDS,REB=TOTAL_REBOUNDS,AST=ASSISTANCES,STL=STEALS,
              TOV=TURNOVERS,BLK=BLOCKS_FAVOUR,PF=FOULS_COMMITED)
   )
-  
 }
-rm(list=setdiff(ls(),c("PP","TT")))
+rm(list=setdiff(ls(),c("PP","TT","fixture_info")))
+
+players = bind_rows(PP)
+teams   = bind_rows(TT)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # beepr::beep()
 # write files in .csv format
-write.csv(bind_rows(PP) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/EC-players.csv")
-
-
-write.csv(bind_rows(TT) %>% mutate(TEAM=toupper(TEAM)),"bball-stats/data/EC-teams.csv")
-
-
-
+write.csv(players,"bball-stats/data/EL-players.csv")
+write.csv(teams,"bball-stats/data/EL-teams.csv")
